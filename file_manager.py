133bacfb37ed0aea391a65e255f28ff24d934731
Pavel Vasiliev <vasilievpavel4@gmail.com>
2023-11-23 03:54:07 +0500

    added file saving

--- file_manager.py
+++ file_manager.py
@@ -3,9 +3,21 @@ import curses
 keys = [curses.KEY_DOWN, curses.KEY_UP, curses.KEY_LEFT, curses.KEY_RIGHT, ord('s')]
 
 
+def parse_data_to_lines(data):
+    result = []
+    for i in range(0, len(data), 16):
+        result_i = []
+        for j in data[i:i + 16]:
+            item = format(j, 'x')
+            if len(item) == 1:
+                item = '0' + item
+            result_i += [item]
+        result += [result_i]
+    return result
+
+
 class FileManager:
     def __init__(self):
-        # TODO: считывать в начале префикс файла
         self.offset = 0
         self.seek_pointer = 0
         self.window_size = 16 * 5
@@ -14,12 +26,9 @@ class FileManager:
 
         self.seek_pointer = len(self.data)
 
-        self.lines = self.parse_data_to_lines(self.data)
+        self.lines = parse_data_to_lines(self.data)
         self.cursor_row = 0
         self.cursor_col = 0
-
-    def parse_data_to_lines(self, data):
-        return [[format(j, 'x') for j in data[i:i + 16]] for i in range(0, len(data), 16)]
 
     def get_formatted_lines(self):
         return [' '.join(str(line[i]) for i in range(0, len(line))) for line in self.lines]
@@ -27,28 +36,31 @@ class FileManager:
     def get_actual_position(self):
         return self.cursor_row, self.cursor_col * 3, self.lines[self.cursor_row][self.cursor_col]
 
+    def get_window_size(self):
+        return sum(len(line) for line in self.lines)
+
     def step_forward_window(self):
         with open('t.txt', 'rb') as f:
             f.seek(self.seek_pointer)
             data = f.read(16)
             self.seek_pointer += len(data)
             if len(data) > 0:
-                lines = self.parse_data_to_lines(data)
+                lines = parse_data_to_lines(data)
                 self.lines = self.lines[1:] + lines
                 return True
             return False
 
     def step_back_window(self):
         with open('t.txt', 'rb') as f:
-            sm = sum(len(line) for line in self.lines)
-            if self.seek_pointer >= sm + 16:
-                self.seek_pointer -= sm + 16
+            sz = self.get_window_size()
+            if self.seek_pointer >= sz + 16:
+                self.seek_pointer -= sz + 16
                 f.seek(self.seek_pointer)
                 data = f.read(16)
 
-                lines = self.parse_data_to_lines(data)
+                lines = parse_data_to_lines(data)
                 self.lines = lines + self.lines[:-1]
-                self.seek_pointer += sum(len(line) for line in self.lines)
+                self.seek_pointer += self.get_window_size()
                 return True
             return False
 
@@ -87,10 +99,14 @@ class FileManager:
                         self.cursor_row += 1
                         self.cursor_col = 0
             elif key == ord('s'):
-                b = self.translate_lines_to_bytes()
-                with open('t.txt', 'wb') as f:
-                    f.seek(self.seek_pointer)
-                    f.write(bytes(b))
+                self.save_file()
+
+    def save_file(self):
+        b = self.translate_lines_to_bytes()
+        with open('t.txt', 'rb+') as f:
+            bb = bytes(b)
+            f.seek(self.seek_pointer - self.get_window_size())
+            f.write(bytes(b))
 
     def change_data(self, key):
         if key not in keys:
@@ -120,5 +136,7 @@ class FileManager:
 #     d = input()
 #     if d == 'b':
 #         verdict = t.step_back_window()
+#     elif d == 's':
+#         t.save_file()
 #     else:
 #         verdict = t.step_forward_window()
